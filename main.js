4b4b57420fc87337a2ffc573691cd3ac7e1e51ae
Sebastian Leañez <133547139+TheSebalex@users.noreply.github.com>
2023-06-27 18:33:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 // Variables
 
-const buttonDeploy = document.getElementById('svg')
-const buttonUnDeploy = document.getElementById('ex')
-const menu = document.getElementById('deploy')
+const buttonDeploy = document.getElementById('svg');
+const buttonUnDeploy = document.getElementById('ex');
+const menu = document.getElementById('deploy');
+
+const contactDeploy = document.getElementById('contact');
+const contactResponsive = document.getElementById('contactMeResponsive')
+const undeployContact = document.getElementById('ex2');
+const menuContact= document.getElementById('contactDeploy')
 
 // Eventos
 
-buttonDeploy.addEventListener('click', agregarClase)
-buttonUnDeploy.addEventListener('click', quitarClase)
-window.addEventListener('scroll', quitarClase)
-window.addEventListener('resize', quitarClase)
+contactResponsive.addEventListener('click', deployedContact)
+contactDeploy.addEventListener('click', deployedContact)
+undeployContact.addEventListener('click', undeployedContact)
+
+buttonDeploy.addEventListener('click', agregarClase);
+buttonUnDeploy.addEventListener('click', quitarClase);
+
+window.addEventListener('scroll', quitarClase);
+window.addEventListener('resize', quitarClase);
+
+// Funciones
 
 function agregarClase(){
     menu.classList.toggle('deployed')
-}
+};
 
 function quitarClase(){
     menu.classList.remove('deployed')
+};
+
+function deployedContact(){
+    menuContact.classList.toggle('contactDeployed')
+}
+
+function undeployedContact(){
+    menuContact.classList.remove('contactDeployed')
 }
